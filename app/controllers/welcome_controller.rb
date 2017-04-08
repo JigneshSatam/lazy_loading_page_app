f5d893589537ea3713bcdd051b61943da418ff86
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-04-08 10:56:11 +0530

    Added callbacks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,19 +3,19 @@ class WelcomeController < ApplicationController
   end
 
   def notifications
-    sleep 1
+    # sleep 1
     # render template: "welcome/notifications", layout: false
   end
 
   def chats
-    sleep 1
+    # sleep 1
     # raise
     # render template: "welcome/notifications", layout: false
   end
 
   def get_classes
     classes = ["fa-user", "fa-user-o", "fa-user-circle-o", "fa-user-circle", "fa-user-times", "fa-user-plus", "fa-user-secret", "fa-users"]
-    sleep 1
+    # sleep 1
     render json:  {fa_class: classes.sample, ele_id: params[:ele_id]}
   end
 end
